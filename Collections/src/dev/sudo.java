f81a94f35b7af9c26b00699f981ace30023bd62d
zahid-01 <erzahidkhan101@gmail.com>
2023-10-05 19:11:52 +0530

    Initial Commit

--- Collections/src/dev/sudo.java
+++ Collections/src/dev/sudo.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.Collection;
 import java.util.LinkedList;
 
 public class sudo {
@@ -9,6 +10,14 @@ public class sudo {
         list.add(2);
         list.add(3);
 
+        LinkedList<Integer> list1 = new LinkedList<>();
+        list1.add(4000);
+        list1.add(2000);
+        list1.add(3000);
+
+        System.out.println(list);
+
+        list.addAll(list1);
         System.out.println(list);
     }
 }
