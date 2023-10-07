5a6f508391ba8cd65afd40d15d5d91cbc5357487
zahid-01 <erzahidkhan101@gmail.com>
2023-10-07 10:54:16 +0530

    Iterators

--- Collections/src/dev/test1/Main.java
+++ Collections/src/dev/test1/Main.java
@@ -2,6 +2,7 @@ package dev.test1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +10,9 @@ public class Main {
         arrayList.addAll(List.of(2,3,4,5));
 
         System.out.println(arrayList);
+        arrayList.add(4,1);
+        System.out.println(arrayList);
+
+        for (Integer integer : arrayList) System.out.println(integer);
     }
 }
